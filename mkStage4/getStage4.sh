5b08a3a7937f9bebf4b7e3a281aa855cdd94b7b3
Matthew Marchese <Maffblaster@gmail.com>
2015-01-30 15:14:02 -0800

    Added bashcheck, mkStage4, and started depcheck

---
+++ mkStage4/getStage4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/DigitalSurvival/mkStage4/master/mkStage4.sh .
+
+chmod +x mkStage4.sh
+
+echo -e "Stage7 has been downloaded and made executable."
+echo -e "You can run it by typing './mkStage4.sh'"
